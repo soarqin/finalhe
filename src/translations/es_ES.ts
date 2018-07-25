49177805e32a57eaa34dc426ec5c14081e93bf11
Z3R0 <34522527+RY0M43CH1Z3N@users.noreply.github.com>
2018-07-26 00:27:19 +0200

    Added Spanish

---
+++ src/translations/es_ES.ts
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>FinalHE</name>
+    <message>
+        <location filename="../main.cc" line="74"/>
+        <source>WARNING</source>
+        <translation type="unfinished">ADVERTENCIA</translation>
+    </message>
+    <message>
+        <location filename="../main.cc" line="74"/>
+        <source>Qcma is running, force close it now?</source>
+        <translation type="unfinished">Qcma está ejecutándose, ¿forzar su cierre?</translation>
+    </message>
+    <message>
+        <location filename="../main.cc" line="78"/>
+        <source>Unable to close Qcma, please close it manually and then restart this tool.</source>
+        <translation type="unfinished">Imposible cerrar Qcma, por favor, ciérrelo manualmente y reinicie esta herramienta.</translation>
+    </message>
+    <message>
+        <location filename="../main.cc" line="78"/>
+        <source>ERROR</source>
+        <translation type="unfinished">ERROR</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have write permission to this folder! Exit now.</source>
+        <translation type="vanished">¡No tienes permiso de escritura en esta carpeta! Finalizando.</translation>
+    </message>
+    <message>
+        <location filename="../finalhe.cc" line="119"/>
+        <source>Registering device: %1
+Input this PIN on PS Vita: %2</source>
+        <translation type="unfinished">Registrando dispositivo: %1
+Inserte este PIN en su PS Vita: %2</translation>
+    </message>
+    <message>
+        <location filename="../finalhe.cc" line="123"/>
+        <source>Registered device.</source>
+        <translation type="unfinished">Dispositivo registrado.</translation>
+    </message>
+</context>
+<context>
+    <name>FinalHEClass</name>
+    <message>
+        <location filename="../finalhe.ui" line="125"/>
+        <source> Let&apos;s GO! </source>
+        <translation type="unfinished">¡Empecemos!</translation>
+    </message>
+    <message>
+        <location filename="../finalhe.ui" line="144"/>
+        <source>Trim h-encore to ~13MB</source>
+        <translation type="unfinished">Reducir h-encore a ~ 13 MB</translation>
+    </message>
+</context>
+<context>
+    <name>Package</name>
+    <message>
+        <location filename="../package.cc" line="96"/>
+        <source>Downloading %1</source>
+        <translation type="unfinished">Descargando %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="117"/>
+        <source>Unpacking %1</source>
+        <translation type="unfinished">Desempaquetando %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="131"/>
+        <source>Failed to unpack %1</source>
+        <translation type="unfinished">Fallo al desempaquetar %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="153"/>
+        <source>Decompressing %1</source>
+        <translation type="unfinished">Descomprimiendo %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="168"/>
+        <source>Failed to decompress %1</source>
+        <translation type="unfinished">Fallo al descomprimir %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="207"/>
+        <source>Verifying %1</source>
+        <translation type="unfinished">Verificando %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="252"/>
+        <source>Everything is ready, now follow below steps on your PS Vita:
+1. Launch Content Manager and connect to your computer.
+2. Select &quot;PC -&gt; PS Vita System&quot; -&gt; &quot;Applications&quot; -&gt; &quot;PS Vita&quot;.
+3. Transfer &quot;h-encore&quot; to your PS Vita.
+4. Run &quot;h-encore&quot; and... Yay, that&apos;s it!</source>
+        <translation type="unfinished">Todo está listo, ahora sigue los pasos a continuación en tu PS Vita:
+1. Inicia Gestor de Contenido y conéctala a tu PC.
+2. Selecciona &quot;PC -&gt; Sistema PS Vita&quot; -&gt; &quot;Aplicación&quot; -&gt; &quot;PS Vita&quot;.
+3. Transfiere &quot;h-encore&quot; a tu PS Vita.
+4. Ejecuta &quot;h-encore&quot; y... Sí, ¡eso es todo!</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="246"/>
+        <source>Fetching backup key from cma.henkaku.xyz</source>
+        <translation type="unfinished">Recuperando clave de copia de seguridad de cma.henkaku.xyz</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="350"/>
+        <source>Fetched backup key.
+Click button to START!</source>
+        <translation type="unfinished">Recuperada clave de copia de seguridad.
+¡Pulsa el botón para EMPEZAR!</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="290"/>
+        <source>Trimming package</source>
+        <translation type="unfinished">Reduciendo el tamaño</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="40"/>
+        <source>Launch Content Manager on PS Vita and connect to computer.</source>
+        <translation type="unfinished">Usa el Gestor de Contenido en tu PS Vita y conéctala al PC.</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="320"/>
+        <source>Createing psvimg&apos;s</source>
+        <translation type="unfinished">Creando el archivo psvimg</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="347"/>
+        <source>Cannot get backup key from your AID.
+Please check your network connection!</source>
+        <translation type="unfinished">No se pudo obtener la clave de tu copia de seguridad de tu AID.
+¡Por favor, compruebe su conexión a internet!</translation>
+    </message>
+</context>
+<context>
+    <name>VitaConn</name>
+    <message>
+        <location filename="../vita.cc" line="356"/>
+        <source>Waiting for connection to PS Vita...</source>
+        <translation type="unfinished">Esperando conexión de PS Vita...</translation>
+    </message>
+    <message>
+        <location filename="../vita.cc" line="359"/>
+        <source>Connected to PS Vita [%1], Waiting for account ID</source>
+        <translation type="unfinished">Conexión a PS Vita [%1], esperando ID de cuenta</translation>
+    </message>
+    <message>
+        <location filename="../vita.cc" line="361"/>
+        <source>Connected to PS Vita [%1] (%2)</source>
+        <translation type="unfinished">Conexión a PS Vita [%1] (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>base</name>
+    <message>
+        <location filename="../finalhe.cc" line="37"/>
+        <location filename="../finalhe.cc" line="132"/>
+        <source>English</source>
+        <translation type="unfinished">Inglés</translation>
+    </message>
+    <message>
+        <location filename="../finalhe.cc" line="45"/>
+        <source>LANGUAGE</source>
+        <translation type="unfinished">Español</translation>
+    </message>
+</context>
+</TS>
