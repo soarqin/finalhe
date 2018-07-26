978a227007e929713db4be6ba45bd0e857c7326b
Hack-Usagi <40888604+Hack-Usagi@users.noreply.github.com>
2018-07-26 23:12:43 +0900

    Added Japanese language

---
+++ src/translations/jp_JP.ts
@@ -0,0 +1,172 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>FinalHE</name>
+    <message>
+        <location filename="../main.cc" line="74"/>
+        <source>WARNING</source>
+        <translation>警告</translation>
+    </message>
+    <message>
+        <location filename="../main.cc" line="74"/>
+        <source>Qcma is running, force close it now?</source>
+        <translation>Qcmaは実行中です。強制終了しますか？</translation>
+    </message>
+    <message>
+        <location filename="../main.cc" line="78"/>
+        <source>Unable to close Qcma, please close it manually and then restart this tool.</source>
+        <translation>Qcmaが強制終了できません。Qcmaを手動で終了してこのソフトを再実行して下さい</translation>
+    </message>
+    <message>
+        <location filename="../main.cc" line="78"/>
+        <source>ERROR</source>
+        <translation>エラー</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have write permission to this folder! Exit now.</source>
+        <translation type="vanished">書き込み権限無し 終了します</translation>
+    </message>
+    <message>
+        <location filename="../finalhe.cc" line="119"/>
+        <source>Registering device: %1
+Input this PIN on PS Vita: %2</source>
+        <translation>デバイス登録: %1
+PSVitaにPINコードを入力して下さい: %2</translation>
+    </message>
+    <message>
+        <location filename="../finalhe.cc" line="123"/>
+        <source>Registered device.</source>
+        <translation>登録されたデバイス</translation>
+    </message>
+</context>
+<context>
+    <name>FinalHEClass</name>
+    <message>
+        <location filename="../finalhe.ui" line="125"/>
+        <source> Let&apos;s GO! </source>
+        <translation>スタート</translation>
+    </message>
+    <message>
+        <location filename="../finalhe.ui" line="144"/>
+        <source>Trim h-encore to ~13MB</source>
+        <translation>h-encoreを約13MBに容量削減</translation>
+    </message>
+</context>
+<context>
+    <name>Package</name>
+    <message>
+        <location filename="../package.cc" line="96"/>
+        <source>Downloading %1</source>
+        <translation>ダウンロード中: %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="117"/>
+        <source>Unpacking %1</source>
+        <translation>複合化中: %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="131"/>
+        <source>Failed to unpack %1</source>
+        <translation>複合化できません %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="154"/>
+        <source>Decompressing %1</source>
+        <translation>展開中: %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="169"/>
+        <source>Failed to decompress %1</source>
+        <translation>展開できません %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="208"/>
+        <source>Verifying %1</source>
+        <translation>チェック中: %1</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="237"/>
+        <source>sha256sum mismatch! Please check your network.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="254"/>
+        <source>Everything is ready, now follow below steps on your PS Vita:
+1. Launch Content Manager and connect to your computer.
+2. Select &quot;PC -&gt; PS Vita System&quot; -&gt; &quot;Applications&quot; -&gt; &quot;PS Vita&quot;.
+3. Transfer &quot;h-encore&quot; to your PS Vita.
+4. Run &quot;h-encore&quot; and... Yay, that&apos;s it!</source>
+        <translation>準備が完了したらPSVitaで以下の手順に従ってください:
+1. コンテンツ管理を実行してPCに接続
+2. 「パソコン->PS Vita」 ->「アプリケーション」 ->「PS Vita」を選択
+3. Vitaにh-encoreをコピー
+4. Vitaで &quot;h-encore&quot;を起動して完了</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="248"/>
+        <source>Fetching backup key from cma.henkaku.xyz</source>
+        <translation>cma.henkaku.xyzからバックアップキーを取得中</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="353"/>
+        <source>Fetched backup key.
+Click button to START!</source>
+        <translation>バックアップキーの取得が完了しました
+スタートをクリックして開始して下さい</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="292"/>
+        <source>Trimming package</source>
+        <translation>容量を削減中</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="40"/>
+        <source>Launch Content Manager on PS Vita and connect to computer.</source>
+        <translation>Vitaでコンテンツ管理を実行</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="322"/>
+        <source>Createing psvimg&apos;s</source>
+        <translation>psvimgを作成中</translation>
+    </message>
+    <message>
+        <location filename="../package.cc" line="350"/>
+        <source>Cannot get backup key from your AID.
+Please check your network connection!</source>
+        <translation>AIDからバックアップキーを取得できません。ネットワーク接続を確認して下さい</translation>
+    </message>
+</context>
+<context>
+    <name>VitaConn</name>
+    <message>
+        <location filename="../vita.cc" line="356"/>
+        <source>Waiting for connection to PS Vita...</source>
+        <translation>PSVitaの接続を待機中</translation>
+    </message>
+    <message>
+        <location filename="../vita.cc" line="359"/>
+        <source>Connected to PS Vita [%1], Waiting for account ID</source>
+        <translation>PSVita[%1]と接続中 アカウントIDを取得中</translation>
+    </message>
+    <message>
+        <location filename="../vita.cc" line="361"/>
+        <source>Connected to PS Vita [%1] (%2)</source>
+        <translation>PSVita [%1] (%2) と接続集</translation>
+    </message>
+</context>
+<context>
+    <name>base</name>
+    <message>
+        <location filename="../finalhe.cc" line="37"/>
+        <location filename="../finalhe.cc" line="132"/>
+        <source>English</source>
+        <translation>英語</translation>
+    </message>
+    <message>
+        <location filename="../finalhe.cc" line="45"/>
+        <source>LANGUAGE</source>
+        <translation>日本語</translation>
+    </message>
+</context>
+</TS>
