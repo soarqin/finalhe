b59c53eaec8fb7f6b0809748b87bace3fee72187
Soar Qin <soarchin@gmail.com>
2018-07-30 21:01:51 +0800

    fixed wrong format of russian language

--- src/translations/ru_RU.ts
+++ src/translations/ru_RU.ts
@@ -22,32 +22,33 @@
         <location filename="../finalhe.cc" line="184"/>
         <source>Registering device: %1
 Input this PIN on PS Vita: %2</source>
-        <translation></translation>
+        <translation>Регистрация устройства: %1
+        	Напишите этот PIN-код на PS Vita</translation>
     </message>
     <message>
         <location filename="../finalhe.cc" line="188"/>
         <source>Registered device.</source>
-        <translation></translation>
+        <translation>Зарегестрированный девайс</translation>
     </message>
     <message>
         <location filename="../main.cc" line="93"/>
         <source>WARNING</source>
-        <translation></translation>
+        <translation>Внимание!</translation>
     </message>
     <message>
         <location filename="../main.cc" line="93"/>
         <source>Qcma is running, force close it now?</source>
-        <translation></translation>
+        <translation>QCMA в данный момент запущен, закрыть ли его принудительно?</translation>
     </message>
     <message>
         <location filename="../main.cc" line="97"/>
         <source>ERROR</source>
-        <translation></translation>
+        <translation>Ошибка</translation>
     </message>
     <message>
         <location filename="../main.cc" line="97"/>
         <source>Unable to close Qcma, please close it manually and then restart this tool.</source>
-        <translation></translation>
+        <translation>Невозможно закрыть QCMA, закройте его вручную и начните процесс заново.</translation>
     </message>
 </context>
 <context>
@@ -55,12 +56,12 @@ Input this PIN on PS Vita: %2</source>
     <message>
         <location filename="../finalhe.ui" line="103"/>
         <source> Let&apos;s GO! </source>
-        <translation></translation>
+        <translation>Поехали!</translation>
     </message>
     <message>
         <location filename="../finalhe.ui" line="122"/>
         <source>Trim h-encore to ~13MB</source>
-        <translation></translation>
+        <translation>Обрезать пузырь h-encore до ~13МБ</translation>
     </message>
 </context>
 <context>
@@ -68,47 +69,47 @@ Input this PIN on PS Vita: %2</source>
     <message>
         <location filename="../package.cc" line="58"/>
         <source>Launch Content Manager on PS Vita and connect to computer.</source>
-        <translation></translation>
+        <translation>Откройте Управление данными на PS Vita и подключите ее к компьютеру</translation>
     </message>
     <message>
         <location filename="../package.cc" line="122"/>
         <source>Downloading %1</source>
-        <translation></translation>
+        <translation>Загрузка %1</translation>
     </message>
     <message>
         <location filename="../package.cc" line="143"/>
         <source>Unpacking %1</source>
-        <translation></translation>
+        <translation>Распаковка %1</translation>
     </message>
     <message>
         <location filename="../package.cc" line="157"/>
         <source>Failed to unpack %1</source>
-        <translation></translation>
+        <translation>Ошибка при распаковке %1</translation>
     </message>
     <message>
         <location filename="../package.cc" line="186"/>
         <source>Decompressing %1</source>
-        <translation></translation>
+        <translation>Сжатие %1</translation>
     </message>
     <message>
         <location filename="../package.cc" line="206"/>
         <source>Failed to decompress %1</source>
-        <translation></translation>
+        <translation>Ошибка при сжатии %1</translation>
     </message>
     <message>
         <location filename="../package.cc" line="296"/>
         <source>Verifying %1</source>
-        <translation></translation>
+        <translation>Проверка %1</translation>
     </message>
     <message>
         <location filename="../package.cc" line="325"/>
         <source>sha256sum mismatch! Please check your network.</source>
-        <translation></translation>
+        <translation>sha256sum несоответствует. Проверьте подключение к интернету.</translation>
     </message>
     <message>
         <location filename="../package.cc" line="401"/>
         <source>Fetching backup key from cma.henkaku.xyz</source>
-        <translation></translation>
+        <translation>Получение ключа от cma.henkaku.xyz</translation>
     </message>
     <message>
         <location filename="../package.cc" line="411"/>
@@ -117,30 +118,35 @@ Input this PIN on PS Vita: %2</source>
 2. Select &quot;PC -&gt; PS Vita System&quot; -&gt; &quot;Applications&quot; -&gt; &quot;PS Vita&quot;.
 3. Transfer &quot;h-encore&quot; to your PS Vita.
 4. Run &quot;h-encore&quot; and... Yay, that&apos;s it!</source>
-        <translation></translation>
+        <translation>Все готово, теперь следуйте этим шагам на вашей PS Vita:
+1. Запустите Управление данными и подключите Vita к компьютеру.
+2. Выберите &quot;Компьютер -&gt; Система PS Vita&quot; -&gt; &quot;Приложения&quot; -&gt; &quot;PS Vita&quot;.
+3. Перенесите &quot;h-encore&quot; на вашу PS Vita.
+4. Запустите &quot;h-encore&quot; и... Ура, все работает!</translation>
     </message>
     <message>
         <location filename="../package.cc" line="447"/>
         <source>Trimming package</source>
-        <translation></translation>
+        <translation>Обрезается пузырь</translation>
     </message>
     <message>
         <location filename="../package.cc" line="482"/>
         <source>Createing psvimg&apos;s</source>
-        <translation></translation>
+        <translation>Создание psvimg</translation>
     </message>
     <message>
         <location filename="../package.cc" line="513"/>
         <source>Cannot get backup key from your AID.
 Please check your network connection!</source>
-        <translation></translation>
+        <translation>Невозможно получить ключ. Проверьте подключение к интернету.</translation>
     </message>
     <message>
         <location filename="../package.cc" line="403"/>
         <location filename="../package.cc" line="516"/>
         <source>Fetched backup key.
 Click button to START!</source>
-        <translation></translation>
+        <translation>Ключ получен
+        	нажмите Начать</translation>
     </message>
 </context>
 <context>
@@ -148,17 +154,17 @@ Click button to START!</source>
     <message>
         <location filename="../vita.cc" line="436"/>
         <source>Waiting for connection to PS Vita...</source>
-        <translation></translation>
+        <translation>Ожидание подключения к PS Vita</translation>
     </message>
     <message>
         <location filename="../vita.cc" line="439"/>
         <source>Connected to PS Vita [%1], Waiting for account ID</source>
-        <translation></translation>
+        <translation>PS Vita [%1] подключена. Ожидание AID</translation>
     </message>
     <message>
         <location filename="../vita.cc" line="441"/>
         <source>Connected to PS Vita [%1] (%2)</source>
-        <translation></translation>
+        <translation>PS Vita [%1] (%2) подключена</translation>
     </message>
 </context>
 <context>
@@ -167,12 +173,12 @@ Click button to START!</source>
         <location filename="../finalhe.cc" line="63"/>
         <location filename="../finalhe.cc" line="246"/>
         <source>English</source>
-        <translation></translation>
+        <translation>Русский</translation>
     </message>
     <message>
         <location filename="../finalhe.cc" line="71"/>
         <source>LANGUAGE</source>
-        <translation></translation>
+        <translation>Язык</translation>
     </message>
 </context>
 </TS>
